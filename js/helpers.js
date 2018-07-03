77727809ed828f9854c63e8faa657e9301643342
Alisa Chernysheva <alisa.chernysheva@veeam.com>
2018-07-03 22:04:42 +0300

    #module7-task1: fixes, edits

--- js/helpers.js
+++ js/helpers.js
@@ -7,6 +7,15 @@
     ENTER: 13
   };
 
+  window.elements = {
+    map: document.querySelector('.map'),
+    form: document.querySelector('.ad-form'),
+    addressField: document.querySelector('#address'),
+    filter: document.querySelector('.map__filters'),
+    pinMain: document.querySelector('.map__pin--main'),
+    pinsList: document.querySelector('.map__pins')
+  };
+
   window.debounce = function (fun, time) {
     var counter;
 
--- js/pins.js
+++ js/pins.js
@@ -1,30 +1,68 @@
 'use strict';
 
-var pinMain = document.querySelector('.map__pin--main');
-var pinsList = document.querySelector('.map__pins');
-var PIN_WIDTH = 50;
-var PIN_HEIGHT = 70;
-var PINS_QUANTITY = 5;
-var pinY = pinMain.offsetTop;
-var pinX = pinMain.offsetLeft;
-var PIN_MAIN_WIDTH = 66;
-var PIN_MAIN_HEIGHT = 74;
-var PIN_INITIAL_X = pinX - PIN_WIDTH / 2;
-var PIN_INITIAL_Y = pinY + PIN_HEIGHT;
-var PIN_INITIAL_POSITIONING = (pinMain.offsetLeft + PIN_WIDTH / 2) + ' , ' + (pinMain.offsetTop - PIN_HEIGHT);
-var Y_MIN_VALUE = 130;
-var Y_MAX_VALUE = 625;
-var MAP_LEFT_X = 0;
-var formElement = document.querySelector('.ad-form');
-var mapElement = document.querySelector('.map');
-var activeMode;
-
 (function () {
+
+  var PINS_QUANTITY = 5;
+  var MAP_LEFT_X = 0;
+  var Y_MIN_VALUE = 130;
+  var Y_MAX_VALUE = 625;
+  var PIN_WIDTH = 50;
+  var PIN_HEIGHT = 70;
+  var PIN_MAIN_WIDTH = 66;
+  var PIN_MAIN_HEIGHT = 74;
+  var pinY = window.elements.pinMain.offsetTop;
+  var pinX = window.elements.pinMain.offsetLeft;
+
+
+  var PIN_MAIN_INITIAL_COORDS = {
+    X: 570,
+    Y: 375
+  };
+
+  window.pins = {
+    PIN_MAIN_INITIAL_COORDS: PIN_MAIN_INITIAL_COORDS,
+    PIN_WIDTH: PIN_WIDTH,
+    PIN_HEIGHT: PIN_HEIGHT,
+    PIN_MAIN_WIDTH: PIN_MAIN_WIDTH,
+    PIN_MAIN_HEIGHT: PIN_MAIN_HEIGHT,
+    pinY: pinY,
+    pinX: pinX,
+
+    generate: function (array) {
+      array.length = array.length > PINS_QUANTITY ? PINS_QUANTITY : array.length;
+
+      for (var i = 0; i < array.length; i++) {
+        var template = document.querySelector('template').content;
+        var sample = template.cloneNode(true);
+        var pin = sample.querySelector('.map__pin');
+        var icon = sample.querySelector('.map__pin-icon');
+
+        pin.style.left = (array[i].location.x + window.pins.PIN_WIDTH / 2) + 'px';
+        pin.style.top = (array[i].location.y - window.pins.PIN_HEIGHT) + 'px';
+        icon.src = array[i].author.avatar;
+
+        window.elements.pinsList.appendChild(pin);
+      }
+    },
+    clear: function () {
+      var pinsSecondary = document.querySelectorAll('.map__pin:not(.map__pin--main)');
+
+      if (pinsSecondary) {
+        for (var i = 0; i < pinsSecondary.length; ++i) {
+          pinsSecondary[i].remove();
+        }
+      }
+    },
+    resetInitialMainPinState: function () {
+      window.elements.pinMain.style.top = window.pins.PIN_MAIN_INITIAL_COORDS.Y + 'px';
+      window.elements.pinMain.style.left = window.pins.PIN_MAIN_INITIAL_COORDS.X + 'px';
+    }
+  };
 
   var pinMainEnterButtonPress = function (evt) {
     evt.preventDefault();
 
-    if ((evt.keyCode === window.keycodes.ENTER) && (!activeMode)) {
+    if ((evt.keyCode === window.keycodes.ENTER) && (!window.map.activeMode)) {
       window.map.enableActiveMode();
     }
   };
@@ -49,37 +87,38 @@ var activeMode;
         y: moveEvt.clientY
       };
 
-      var PIN_FINAL_Y = pinMain.offsetTop - shift.y;
-      var PIN_FINAL_X = pinMain.offsetLeft - shift.x;
+      var PIN_FINAL_Y = window.elements.pinMain.offsetTop - shift.y;
+      var PIN_FINAL_X = window.elements.pinMain.offsetLeft - shift.x;
 
       if (PIN_FINAL_Y >= Y_MAX_VALUE) {
-        pinMain.style.top = Y_MAX_VALUE + 'px';
+        window.elements.pinMain.style.top = Y_MAX_VALUE + 'px';
       } else if (PIN_FINAL_Y <= Y_MIN_VALUE) {
-        pinMain.style.top = Y_MIN_VALUE + 'px';
+        window.elements.pinMain.style.top = Y_MIN_VALUE + 'px';
       } else {
-        pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
+        window.elements.pinMain.style.top = (window.elements.pinMain.offsetTop - shift.y) + 'px';
       }
 
-      // left positioning of the dragged pin
-      var box = mapElement.getBoundingClientRect();
+      var box = window.elements.map.getBoundingClientRect();
 
       if (PIN_FINAL_X <= MAP_LEFT_X) {
-        pinMain.style.left = MAP_LEFT_X;
-      } else if (PIN_FINAL_X >= (box.width - PIN_WIDTH)) {
-        pinMain.style.left = (box.width - PIN_WIDTH) + 'px';
+        window.elements.pinMain.style.left = MAP_LEFT_X;
+      } else if (PIN_FINAL_X >= (box.width - PIN_MAIN_WIDTH)) {
+        window.elements.pinMain.style.left = (box.width - PIN_MAIN_WIDTH) + 'px';
       } else {
-        pinMain.style.left = PIN_FINAL_X + 'px';
+        window.elements.pinMain.style.left = PIN_FINAL_X + 'px';
       }
 
-      inputAdress.value = ((pinMain.offsetLeft - shift.x) + PIN_WIDTH / 2) + ' , ' + ((pinMain.offsetTop - shift.y) - PIN_HEIGHT);
+      window.elements.addressField.value = ((window.elements.pinMain.offsetLeft - shift.x) + PIN_MAIN_WIDTH / 2) + ' , ' + ((window.elements.pinMain.offsetTop - shift.y) - PIN_MAIN_HEIGHT);
 
-      if (!activeMode) {
+      if (!window.map.activeMode) {
         window.map.enableActiveMode();
       }
     };
 
     var onMouseUp = function (upEvt) {
       upEvt.preventDefault();
+
+      window.elements.addressField.value = (window.elements.pinMain.offsetLeft + PIN_MAIN_WIDTH / 2) + ' , ' + (window.elements.pinMain.offsetTop + PIN_MAIN_HEIGHT);
 
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
@@ -88,34 +127,7 @@ var activeMode;
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   };
-  pinMain.addEventListener('mousedown', pinMainHandle);
-  pinMain.addEventListener('keydown', pinMainEnterButtonPress);
+  window.elements.pinMain.addEventListener('mousedown', pinMainHandle);
+  window.elements.pinMain.addEventListener('keydown', pinMainEnterButtonPress);
 
-  window.pins = {
-    generate: function (array) {
-      array.length = array.length > PINS_QUANTITY ? PINS_QUANTITY : array.length;
-
-      for (var i = 0; i < array.length; i++) {
-        var template = document.querySelector('template').content;
-        var sample = template.cloneNode(true);
-        var pin = sample.querySelector('.map__pin');
-        var icon = sample.querySelector('.map__pin-icon');
-
-        pin.style.left = (array[i].location.x + PIN_WIDTH / 2) + 'px';
-        pin.style.top = (array[i].location.y - PIN_HEIGHT) + 'px';
-        icon.src = array[i].author.avatar;
-
-        pinsList.appendChild(pin);
-      }
-    },
-    clear: function () {
-      var pinsSecondary = document.querySelectorAll('.map__pin:not(.map__pin--main)');
-
-      if (pinsSecondary) {
-        for (var i = 0; i < pinsSecondary.length; ++i) {
-          pinsSecondary[i].remove();
-        }
-      }
-    }
-  };
 })();
